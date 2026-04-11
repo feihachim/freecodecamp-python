2898574db689004c36209e60a50a6a9729b1b2c7
Hachim Youssoufa <feihachim@yahoo.fr>
2026-04-12 00:12:14 +0200

    lab number pattern generator done

--- 02 Loops and Sequences/Lab/number-pattern-generator.py
+++ 02 Loops and Sequences/Lab/number-pattern-generator.py
@@ -1,1 +1,15 @@
-print("TODO")
+"""
+In this lab you will practice the basics of Python by building a small app that creates a number pattern.
+"""
+
+
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+    result = ""
+    for i in range(1, n):
+        result += str(i) + " "
+    result += str(n)
+    return result
