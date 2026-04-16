f534c3eb7f8f725e85767c5ce56b46118ff73940
Hachim Youssoufa <feihachim@yahoo.fr>
2026-04-17 00:24:06 +0200

    lab game character stats tracker done

--- 06 Object-Oriented Programming/labs/game-character-stats-tracker.py
+++ 06 Object-Oriented Programming/labs/game-character-stats-tracker.py
@@ -1,1 +1,54 @@
-print("TODO")
+"""
+In this lab, you'll build a game character stats tracker.
+The program will allow you to create a character with specific attributes,
+update those attributes, and retrieve the current stats of the character.
+"""
+
+
+class GameCharacter:
+    def __init__(self, name):
+        self._name = name
+        self._health = 100
+        self._mana = 50
+        self._level = 1
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, new_health):
+        if new_health < 0:
+            new_health = 0
+        if new_health > 100:
+            new_health = 100
+        self._health = new_health
+
+    @property
+    def mana(self):
+        return self._mana
+
+    @mana.setter
+    def mana(self, new_mana):
+        if new_mana < 0:
+            new_mana = 0
+        if new_mana > 50:
+            new_mana = 50
+        self._mana = new_mana
+
+    @property
+    def level(self):
+        return self._level
+
+    def level_up(self):
+        self._level += 1
+        self.health = 100
+        self.mana = 50
+        print(f"{self.name} leveled up to {self._level}!")
+
+    def __str__(self):
+        return f"Name: {self.name}\nLevel: {self.level}\nHealth: {self.health}\nMana: {self.mana}"
