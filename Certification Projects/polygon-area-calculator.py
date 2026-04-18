63927443c24dc3ccf9427cc2d3b9100639540701
Hachim Youssoufa <feihachim@yahoo.fr>
2026-04-18 21:43:56 +0200

    update project file

--- Certification Projects/polygon-area-calculator.py
+++ Certification Projects/polygon-area-calculator.py
@@ -5,29 +5,29 @@ The Square class should be a subclass of Rectangle and inherit its methods and a
 
 
 class Rectangle:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Rectangle(width={self.width}, height={self.height})"
 
-    def get_area(self):
+    def get_area(self) -> int:
         return self.width * self.height
 
-    def get_perimeter(self):
+    def get_perimeter(self) -> int:
         return 2 * (self.width + self.height)
 
-    def get_diagonal(self):
+    def get_diagonal(self) -> float:
         return (self.width**2 + self.height**2) ** 0.5
 
-    def set_width(self, new_width):
+    def set_width(self, new_width: int) -> None:
         self.width = new_width
 
-    def set_height(self, new_height):
+    def set_height(self, new_height: int) -> None:
         self.height = new_height
 
-    def get_picture(self):
+    def get_picture(self) -> str:
         if self.width > 50 or self.height > 50:
             return "Too big for picture."
         picture = ""
@@ -37,7 +37,7 @@ class Rectangle:
             picture += "\n"
         return picture
 
-    def get_amount_inside(self, figure):
+    def get_amount_inside(self, figure: "Rectangle") -> int:
         min_self = min(self.width, self.height)
         max_self = max(self.width, self.height)
         min_figure = min(figure.width, figure.height)
@@ -46,22 +46,22 @@ class Rectangle:
 
 
 class Square(Rectangle):
-    def __init__(self, side):
+    def __init__(self, side: int) -> None:
         self.side = side
         super().__init__(side, side)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Square(side={self.side})"
 
-    def set_side(self, new_side):
+    def set_side(self, new_side: int) -> None:
         self.side = new_side
         super().set_width(new_side)
         super().set_height(new_side)
 
-    def set_width(self, new_width):
+    def set_width(self, new_width: int) -> None:
         self.set_side(new_width)
 
-    def set_height(self, new_height):
+    def set_height(self, new_height: int) -> None:
         self.set_side(new_height)
 
 
