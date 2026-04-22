1bdd1df42cd0fab998c7e6639aabed8aa2e53bfa
Hachim Youssoufa <feihachim@yahoo.fr>
2026-04-23 00:40:15 +0200

    workshop shortest path done

--- 06 Object-Oriented Programming/workshops/salary-tracker.py
+++ 06 Object-Oriented Programming/workshops/salary-tracker.py
@@ -12,13 +12,9 @@ class Employee:
     }
 
     def __init__(self, name, level):
-        if not (isinstance(name, str) and isinstance(level, str)):
-            raise TypeError("'name' and 'level' attribute must be of type 'str'.")
-        if level not in Employee._base_salaries:
-            raise ValueError(f"Invalid value '{level}' for 'level' attribute.")
-        self._name = name
-        self._level = level
-        self._salary = Employee._base_salaries[level]
+        self.name = name
+        self.level = level
+        self.salary = Employee._base_salaries[level]
 
     def __str__(self):
         return f"{self.name}: {self.level}"
@@ -43,11 +39,16 @@ class Employee:
 
     @level.setter
     def level(self, new_level):
+        if not isinstance(new_level, str):
+            raise TypeError("'level' must be a string.")
         if new_level not in Employee._base_salaries:
             raise ValueError(f"Invalid value '{new_level}' for 'level' attribute.")
-        if new_level == self.level:
+        if hasattr(self, "_level") and new_level == self.level:
             raise ValueError(f"'{self.level}' is already the selected level.")
-        if Employee._base_salaries[new_level] < Employee._base_salaries[self.level]:
+        if (
+            hasattr(self, "_level")
+            and Employee._base_salaries[new_level] < Employee._base_salaries[self.level]
+        ):
             raise ValueError("Cannot change to lower level.")
         print(f"'{self.name}' promoted to '{new_level}'.")
         self.salary = Employee._base_salaries[new_level]
@@ -61,7 +62,7 @@ class Employee:
     def salary(self, new_salary):
         if not isinstance(new_salary, (int, float)):
             raise TypeError("'salary' must be a number.")
-        if new_salary <= Employee._base_salaries[self.level]:
+        if hasattr(self, "_level") and new_salary < Employee._base_salaries[self.level]:
             raise ValueError(
                 f"Salary must be higher than minimum salary ${Employee._base_salaries[self.level]}."
             )
