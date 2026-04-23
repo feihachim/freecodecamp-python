f4b4d3cc303a6b406cffbafb059ed886cd6dc338
Hachim Youssoufa <feihachim@yahoo.fr>
2026-04-23 23:08:36 +0200

    update fibonacci calculator

--- 10 Dynamic Programming/lab/nth-fibonacci-number-calculator.py
+++ 10 Dynamic Programming/lab/nth-fibonacci-number-calculator.py
@@ -4,10 +4,18 @@ Build an Nth Fibonacci Number Calculator
 
 
 def fibonacci(n: int) -> int:
-    if n == 0 or n == 1:
-        return n
-    sequence = [0, 1]
+    if n < 0:
+        return 0
+    sequence = [0] * (n + 1)
+    sequence[1] = 1
     for i in range(2, n + 1):
-        fibn = sequence[i - 1] + sequence[i - 2]
-        sequence.append(fibn)
+        sequence[i] = sequence[i - 1] + sequence[i - 2]
     return sequence[n]
+
+
+if __name__ == "__main__":
+    print(fibonacci(3))
+    print(fibonacci(5))
+    print(fibonacci(7))
+    print(fibonacci(15))
+    print(fibonacci(40))
